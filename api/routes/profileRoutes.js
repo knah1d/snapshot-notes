8e37362826996db98846a9567df7c389c75cd45d
knah1d <bsse1437@iit.du.ac.bd>
2025-05-22 11:41:40 +0600

    style: format profileRoutes.js for consistent quotation marks

--- api/routes/profileRoutes.js
+++ api/routes/profileRoutes.js
@@ -1,20 +1,24 @@
-
-import express from 'express';
-import { protect } from '../middlewares/auth.js';
-import { getProfile, updatePassword, deleteAccount, getUserStats } from '../controllers/profileController.js';
+import express from "express";
+import { protect } from "../middlewares/auth.js";
+import {
+    getProfile,
+    updatePassword,
+    deleteAccount,
+    getUserStats,
+} from "../controllers/profileController.js";
 
 const router = express.Router();
 
 // Get user profile
-router.get('/me', protect, getProfile);
+router.get("/me", protect, getProfile);
 
 // Update user password
-router.put('/update-password', protect, updatePassword);
+router.put("/update-password", protect, updatePassword);
 
 // Delete user account
-router.delete('/delete-account', protect, deleteAccount);
+router.delete("/delete-account", protect, deleteAccount);
 
 // Get user stats
-router.get('/stats', protect, getUserStats);
+router.get("/stats", protect, getUserStats);
 
 export default router;
